76043f233845f1fb7b0ed9f229d7ae05d5af46f9
leochilds <leo.childs@live.com>
2020-09-23 11:18:42 +0100

    endpoint

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT ? process.env.PORT : 3000;
+
+app.get('/api/timestamp/:date_string?', function (req, res) {
+    res.send(req.params);
+})
+
+app.listen(port, () => {
+    console.log(`Timestamp Microservice listening at http://localhost:${port}`)
+})
+
 const getDateTimeFromString = (date_string) => {
     let date = date_string ? new Date(date_string) : new Date();
     return date;
